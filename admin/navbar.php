15303d52bca60ca5fded6798305de4c21d72a98e
amnzhar <160093669+amnzhar@users.noreply.github.com>
2024-02-15 13:12:17 +0800

    Add files via upload

---
+++ admin/navbar.php
@@ -0,0 +1,31 @@
+
+<style>
+	.collapse a{
+		text-indent:10px;
+	}
+	nav#sidebar{
+		background: url(assets/uploads/<?php echo $_SESSION['system']['cover_img'] ?>) !important
+	}
+</style>
+
+<nav id="sidebar" class='mx-lt-5 bg-dark' >
+		
+		<div class="sidebar-list">
+				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+				<a href="index.php?page=manage_event" class="nav-item nav-audience"><span class='icon-field'><i class="fa fa-file"></i></span> Create Event</a>
+				<a href="index.php?page=events" class="nav-item nav-events"><span class='icon-field'><i class="fa fa-th-list"></i></span> Event List</a>
+				<!-- <a href="index.php?page=venue" class="nav-item nav-venue"><span class='icon-field'><i class="fa fa-map-marked-alt"></i></span> Venues</a>			
+				<?php if($_SESSION['login_type'] == 1): ?>
+				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
+				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> System Settings</a>
+			<?php endif; ?> -->
+		</div>
+
+</nav>
+<script>
+	$('.nav_collapse').click(function(){
+		console.log($(this).attr('href'))
+		$($(this).attr('href')).collapse()
+	})
+	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+</script>
